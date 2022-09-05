4fcfe34e9821567acf5f5fcc97625e69c92d9e09
SangYun0922 <skwx50000@gmail.com>
2022-09-05 15:21:19 +0900

    Add : jsp + SpringBoot Chatting System

--- chat/chat-server/src/main/java/com/example/codetattoochat/config/WebSocketConfig.java
+++ chat/chat-server/src/main/java/com/example/codetattoochat/config/WebSocketConfig.java
@@ -1,4 +1,23 @@
 package com.example.codetattoochat.config;
 
-public class WebSocketConfig {
+import com.example.codetattoochat.handler.SocketHandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer{
+
+    @Autowired
+    SocketHandler socketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(socketHandler, "/chating");
+    }
 }
