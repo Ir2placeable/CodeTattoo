d42530008ecb7508b3e20e5e133fd07a58e07cd0
SangYun0922 <skwx50000@gmail.com>
2022-09-05 22:32:38 +0900

    Add : dto, vo, entity 디렉터리 추가

--- chat/chat-server/src/main/java/com/example/codetattoochat/controller/ChatController.java
+++ chat/chat-server/src/main/java/com/example/codetattoochat/controller/ChatController.java
@@ -1,8 +1,11 @@
 package com.example.codetattoochat.controller;
 
 import com.example.codetattoochat.service.APIInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +15,7 @@ import java.time.LocalDateTime;
 
 @Controller
 @RestController
+@Slf4j
 public class ChatController {
 
     @RequestMapping("/chat")
@@ -25,5 +29,12 @@ public class ChatController {
     public APIInfo ping() {
         APIInfo info = APIInfo.builder().app("CodeTattoo-Chat").ver("1.0").timestamp(LocalDateTime.now()).build();
         return info;
+    }
+
+    @GetMapping("/chat/userlist/{sender}")
+    public ResponseEntity getUserList(@PathVariable String sender) {
+        log.debug("UserList sender : {}", sender);
+
+
     }
 }
