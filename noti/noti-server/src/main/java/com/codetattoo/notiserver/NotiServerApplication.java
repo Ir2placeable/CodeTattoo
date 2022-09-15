7c32cbfae1ee564e828f441c5a2324b69e7025de
Sangyun Kim <74143534+SangYun0922@users.noreply.github.com>
2022-09-15 21:40:10 +0900

    Add : noti

---
+++ noti/noti-server/src/main/java/com/codetattoo/notiserver/NotiServerApplication.java
@@ -0,0 +1,13 @@
+package com.codetattoo.notiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotiServerApplication.class, args);
+	}
+
+}
