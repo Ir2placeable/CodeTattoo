c8b20642aa7cd1881604ae6316345c7acf61ea88
ir2placeable <zxcv123594@gmail.com>
2022-10-21 17:02:02 +0900

    hotfix

--- blockchain/codeTattoo.js
+++ blockchain/codeTattoo.js
@@ -19,14 +19,14 @@ class CodeTattoo extends Contract {
         console.info('============= END : Initialize Ledger ===========');
     }
 
-    async newTattoo(ctx, key, owner_info, timestamp) {
+    async newTattoo(ctx, key, owner_info, timestamp, cost, image, body_part) {
         const new_tattoo = {
             state : 1,
             activator_id : owner_info,
             timestamp : timestamp,
-            cost : "",
-            image : "",
-            body_part : "",
+            cost : cost,
+            image : image,
+            body_part : body_part,
             inks : "",
             niddle : "",
             depth : "",
